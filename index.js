d2bd5e97c2e5892073f7d426d4e67207b877dc5e
Dylan Dewey <dylandewey41@gmail.com>
2020-07-22 15:32:56 -0600

    adding questions and selections

---
+++ index.js
@@ -0,0 +1,51 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of this project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Give a short description of your project',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Table of Contents',
+            name: 'Table of Contents'
+        },
+        {
+            type: 'input',
+            message: 'Any special instructions for installation',
+            name: 'Installation'
+        },
+        {
+            type: 'input',
+            message: 'How does one run this file?',
+            name: 'usage'
+        },
+        {
+            type: 'list',
+            message: 'What type of license would you like to have for this readme?',
+            name: 'License'
+            choices: [
+                'MIT',
+                'Drivers',
+                'Pilots'
+            ]
+        },
+        {
+            type: 'input',
+            message: 'Who are the contributors to this project?',
+            name: 'Contributors'
+        },
+        {
+            type: 'input',
+            message: ''
+        }
+
+
+    ])
